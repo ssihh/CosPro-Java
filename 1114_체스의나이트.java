a9fd199ec55053f92e067d9ae656303f3b5ef1c4
ssihh <49466178+ssihh@users.noreply.github.com>
2023-11-14 21:47:36 +0900

    Update 1114_체스의나이트.java

--- 1114_체스의나이트.java
+++ 1114_체스의나이트.java
@@ -1,23 +1,23 @@
 // 다음과 같이 import를 사용할 수 있습니다.
 import java.util.*;
 class Main {
-    public int solution(String pos) {
-        int a= 8-((int)pos.charAt(1)-48); //7 .1
-				int b= (int)pos.charAt(0)-65; //A .0
-				// System.out.println(a);System.out.println(b);
-
-				int[][] c = new int[8][8];	
+    public int solution(String pos) {        
+	int a= 8-(pos.charAt(1)-'0'); //7 .1
+	int b= pos.charAt(0)-'A'; //A .0
+	// System.out.println(a);System.out.println(b);
+	
+	int[][] c = new int[8][8];	
         int answer = 0;
-				
-			//정각 시계방향으로
-			if(a-2>-1 && b+1<8) answer++; 
-			if(a-1>-1 && b+2<8) answer++;
-			if(a+1<8 && b+2<8) answer++;
-			if(a+2<8 && b+1<8) answer++;
-			if(a+2<8 && b-1>-1) answer++;
-			if(a+1<8 && b-2>-1) answer++;
-			if(a-1>-1 && b-2>-1) answer++;
-			if(a-2>-1 && b-1>-1) answer++;
+		
+	//정각 시계방향으로
+	if(a-2>-1 && b+1<8) answer++; 
+	if(a-1>-1 && b+2<8) answer++;
+	if(a+1<8 && b+2<8) answer++;
+	if(a+2<8 && b+1<8) answer++;
+	if(a+2<8 && b-1>-1) answer++;
+	if(a+1<8 && b-2>-1) answer++;
+	if(a-1>-1 && b-2>-1) answer++;
+	if(a-2>-1 && b-1>-1) answer++;
 				
         return answer;
     }    // 아래는 테스트케이스 출력을 해보기 위한 main 메소드입니다.
