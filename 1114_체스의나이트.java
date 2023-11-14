acb208a41e60338cedeaca7266de213e91faa944
ssihh <49466178+ssihh@users.noreply.github.com>
2023-11-14 19:56:17 +0900

    Create 1114_체스의나이트.java

---
+++ 1114_체스의나이트.java
@@ -0,0 +1,32 @@
+// 다음과 같이 import를 사용할 수 있습니다.
+import java.util.*;
+class Main {
+    public int solution(String pos) {
+        int a= 8-((int)pos.charAt(1)-48); //7 .1
+				int b= (int)pos.charAt(0)-65; //A .0
+				// System.out.println(a);System.out.println(b);
+
+				int[][] c = new int[8][8];	
+        int answer = 0;
+				
+			//정각 시계방향으로
+			if(a-2>-1 && b+1<8) answer++; 
+			if(a-1>-1 && b+2<8) answer++;
+			if(a+1<8 && b+2<8) answer++;
+			if(a+2<8 && b+1<8) answer++;
+			if(a+2<8 && b-1>-1) answer++;
+			if(a+1<8 && b-2>-1) answer++;
+			if(a-1>-1 && b-2>-1) answer++;
+			if(a-2>-1 && b-1>-1) answer++;
+				
+        return answer;
+    }    // 아래는 테스트케이스 출력을 해보기 위한 main 메소드입니다.
+    public static void main(String[] args) {
+        Main sol = new Main();
+        String pos = "A7";
+        int ret = sol.solution(pos);
+    	
+        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+        System.out.println("solution 메소드의 반환 값은 " + ret + " 입니다.");
+    }
+}
