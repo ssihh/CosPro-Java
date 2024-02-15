f72f9319da5a69600027871cc510d75257d91be3
ssihh <49466178+ssihh@users.noreply.github.com>
2024-02-15 21:50:36 +0900

    Update 9_모음사전(DFS).java

--- 23.11/9_모음사전(DFS).java
+++ 23.11/9_모음사전(DFS).java
@@ -1,33 +1,35 @@
-import java.util.*; //에러뜨면 import 해보자
+import java.util.*; // 이유모를 에러뜨면 import 해보자
 
 class Solution {
-    String[] alpha = {"A", "E", "I", "O", "U"};
-    ArrayList<String> sazon; 
-    //List 인터페이스를 구현한 클래스이기 때문에 저장된 값의 순서를 유지하고 중복을 허용한다. 
     
-    public void create_words(int lev, String str) { //0,""
-        sazon.add(str); //words.get(i) = "" : 첫번째(0) 값은 ""임
-        for (int i = 0; i < 5; i++) {
-            if (lev < 5) { //lev 0
-                create_words(lev+1, str.concat(alpha[i])); //재귀함수
+    String[] alpha = {"A", "E", "I", "O", "U"};
+    ArrayList<String> sazon = new ArrayList<>(); //List 인터페이스를 구현한 클래스이기 때문에 저장된 값의 순서를 유지하고 중복을 허용한다. 
+    
+    public void DFS(int len, String str){
+        sazon.add(str); //사전에 값추가. //words.get(i) = "" : 첫번째(0) 값은 ""임
+          
+        for(int i=0; i<5; i++){
+            if(len<5){
+                DFS(len+1, str.concat(alpha[i]));
             }
-            else break; // lev(문자길이) 5면 for문 탈출 굳이 i 다 돌필요없음.
+            else{
+                break; // lev(문자길이) 5면 for문 탈출 굳이 i 다 돌필요없음.
+            }
         }
     }
     
     public int solution(String word) {
         int answer = 0;
-        sazon = new ArrayList<>();
         
-        create_words(0, ""); //DFS 함수 호출
+        DFS(0, ""); //0번째값 공란. DFS 함수 호출
         
-        for (int i = 0; i < sazon.size(); i++) {
-            if (word.equals(sazon.get(i))) { //입력과 사전.get(i) 번째 값이 같으면
-                answer = i; //i번째 값에 넣어주고
+        for(int i=0; i<sazon.size(); i++){ //입력과 사전.get(i) 번째 값이 같으면
+            if(word.equals(sazon.get(i))){ //i번째 값에 넣어주고
+                answer = i;
                 break;
             }
         }
+        
         return answer;
     }
 }
-
